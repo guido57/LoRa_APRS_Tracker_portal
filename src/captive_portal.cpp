8cf8051b0f1710167dc44cbbbf717adc4f64c1ff
guido57 <guido@dogsally.com>
2024-05-05 21:03:21 +0200

    Update captive_portal.cpp

--- src/captive_portal.cpp
+++ src/captive_portal.cpp
@@ -2,6 +2,7 @@
 #include <config.h>
 #include <handleHttp.h>
 #include <WebServer.h>
+#include <logger.h>
 
 // the ConfigClass 
 extern Config_ns::ConfigClass cc; 
@@ -49,14 +50,18 @@ void connectWifi() {
     Serial.println("Connecting as wifi client...");
     //WiFi.forceSleepWake();
     //WiFi.disconnect();
-    /*
-    Serial.print("ssid=");Serial.println( espNow->settings.entries.ssid);
-    Serial.print("password=");Serial.println(espNow->settings.entries.pwd);
-    WiFi.begin(espNow->settings.entries.ssid, espNow->settings.entries.pwd);
+    WiFi.mode(WIFI_MODE_AP);
+    logPrintI("Connecting to ssid="); logPrintlnI(cc["WiFiSsid"]->val_string);
+    WiFi.begin(cc["WiFiSsid"]->val_string.c_str(), cc["WiFiPwd"]->val_string.c_str());
     int connRes = WiFi.waitForConnectResult();
-    Serial.print("connRes: ");
-    Serial.println(connRes);
-    */
+    if(connRes == WL_CONNECTED){
+      logPrintI("Connected to "); logPrintI(cc["WiFiSsid"]->val_string);  
+      logPrintI(" IP="); logPrintlnI(WiFi.localIP().toString());
+        
+    }else{
+      logPrintD("connRes: ");
+      logPrintlnD(String(connRes));
+    }
 }
 
 
@@ -73,22 +78,60 @@ void printLocalTime()
   tm timeinfo;
   bool got_local_time = false;
 
+  unsigned long next_try_connect_millis = 0L;
+  unsigned long try_connect_period_millis = 60000L; // 60 seconds
+
+  unsigned long next_try_DNS_millis = 0L;
+  unsigned long try_DNS_period_millis = 10; // 10 milliseconds
+
+  bool captive_portal_setup_ok = false;
+  bool access_point_setup_ok = false;
 // =====================================================
-// Callback for the TaskWIFI  
 /**
-   check WIFI conditions and try to connect to WIFI.
+   1) if enabled, check WIFI conditions and try to connect to WIFI
+   2) if enabled, run Access Point 
  * @return void
  */
-void WiFi_loop(void){
+void WiFi_loop(bool access_point_enabled){
+  
+  bool wifi_enabled = cc["WiFiEnabled"]->val_bool; 
 
-  //DNS
-  dnsServer.processNextRequest();
-  //HTTP
-  web_server.handleClient();
+  if(access_point_enabled && access_point_setup_ok == false ){
+    AccessPointSetup();
+    access_point_setup_ok = true;
+  }
 
+
+  if( (WiFi.isConnected() || access_point_enabled) && captive_portal_setup_ok == false ){
+    CaptivePortalSetup();
+    captive_portal_setup_ok = true;
+  }
+
+
+  if(wifi_enabled && ! WiFi.isConnected() && millis() > next_try_connect_millis ){
+    //Serial.printf("Try to connect to WiFi\r\n");
+    connectWifi();
+
+    next_try_connect_millis = millis() + try_connect_period_millis;
+  }
+
+  if(access_point_enabled){
+
+      //DNS
+      dnsServer.processNextRequest();
+  }
+
+  if(access_point_enabled || wifi_enabled){
+    
+    if(millis() > next_try_DNS_millis){
+      //HTTP
+      web_server.handleClient();
+      next_try_DNS_millis = millis() + try_DNS_period_millis;
+    }
+  }
 }
 
-// Set the Access Point but let the WiFi off
+// Set the Access Point
 void AccessPointSetup(){
 
   //softAP_ssid =  "ESP32_" + WiFi.macAddress();
@@ -104,21 +147,19 @@ void AccessPointSetup(){
   delay(2000);
   WiFi.softAPConfig(apIP, apIP, netMsk);
   delay(100); // Without delay I've seen the IP address blank
-  Serial.println("Access Point set:");
+  logPrintI("Access Point set! SSID: ");logPrintI(softAP_ssid);
+  logPrintI(" IP address: "); logPrintlnI(WiFi.softAPIP().toString()); 
   
-  Serial.printf("    SSID: %s\r\n", softAP_ssid.c_str());
-  Serial.print("    IP address: ");
-  Serial.println(WiFi.softAPIP());
-}
-
-void CaptivePortalSetup(){
-
-  AccessPointSetup();
-
   /* Setup the DNS web_server redirecting all the domains to the apIP */
   dnsServer.setErrorReplyCode(DNSReplyCode::NoError);
   dnsServer.start(DNS_PORT, "*", apIP);
+  
+}
 
+// Set the DNS Server and the Web Server
+void CaptivePortalSetup(){
+  
+  
   /* Setup web pages: root, wifi config pages, SO captive portal detectors and not found. */
   web_server.on("/", handlehttp::handleRoot);
   web_server.on("/settingssave", handlehttp::handleSettingsSave);
@@ -129,7 +170,7 @@ void CaptivePortalSetup(){
   web_server.on("/fwlink", handlehttp::handleRoot);  //Microsoft captive portal. Maybe not needed. Might be handled by notFound handler.
   web_server.onNotFound(handlehttp::handleNotFound);
   web_server.begin(); // Web server start
-  Serial.println("HTTP server started");
+  logPrintlnI("HTTP server started");
   //loadCredentials(); // Load WLAN credentials from network
   //connect = strlen(espNow->settings.entries.ssid) > 0; // Request WLAN connect if there is a SSID
 }
